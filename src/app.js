2c67e8658d845ad742c2fa96c0c2f3010d965efe
Lucas Tavares <l.tavaresc@hotmail.com>
2022-09-14 17:55:31 -0300

    Added the function to add items to cart

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
-import productsRoutes from "./routes/product.routes.js";
+import productsRoutes from "./routes/products.routes.js";
 dotenv.config();
 
 const app = express();
--- src/routes/product.routes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { listProducts } from "../controllers/products.controller.js";
-
-const router = express.Router();
-
-router.get("/products", listProducts);
-
-export default router;
