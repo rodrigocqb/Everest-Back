cacc307bb3526853f12496ebf532fccc75b6dbe9
Lucas Tavares <l.tavaresc@hotmail.com>
2022-09-13 22:28:27 -0300

    Organização inicial dos arquivos.

---
+++ src/database/db.js
@@ -0,0 +1,15 @@
+import dotenv from 'dotenv';
+import { MongoClient } from 'mongodb';
+dotenv.config();
+
+const mongoClient = new MongoClient(process.env.MONGO_URI)
+
+try {
+    await mongoClient.connect();
+    console.log("MongoDB connected");
+} catch (error) {
+    console.log(error.message);
+}
+const db = mongoClient.db('everest');
+
+export default db
