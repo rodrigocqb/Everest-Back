a89c4220653ddfff34179a09d2705e07e680658a
Lucas Tavares <l.tavaresc@hotmail.com>
2022-09-14 00:32:44 -0300

    Agora o app salva a data da criação da conta do usuario.

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import db from '../database/db.js';
 import bcrypt from 'bcrypt';
 import joi from 'joi';
+import dayjs from 'dayjs';
 
 const createUserSchema = joi.object({
     name: joi.string().required().empty(),
@@ -13,19 +14,27 @@ async function createUser(req, res) {
     if (validation.error) {
         res.sendStatus(422);
     }
-    const { name, email, password } = req.body;
+    const { name, password } = req.body;
+    const email = req.body.email.toLowerCase()
 
     const userData = {
         name,
         email,
-        password: bcrypt.hashSync(password, 10)
+        password: bcrypt.hashSync(password, 10),
+        date: dayjs().format('DD/MM/YYYY')
     };
 
     try {
+        const hasUser = await db.collection('users').findOne({email: email});
+
+        if(hasUser) {
+            return res.sendStatus(409)
+        };
+
         await db.collection('users').insertOne(userData);
-        res.sendStatus(201)
+        res.sendStatus(201);
     } catch (error) {
-        res.status(500).send(error.message)
+        res.status(500).send(error.message);
     }
 }
 
