b833514ed5db1319e20459603c86e7951b1138b5
rodrigocqb <rodrigocortibarros@gmail.com>
2022-09-14 15:07:42 -0300

    Changing import route on auth.routes for new file name

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createUser, login } from "../controllers/user.controller.js";
+import { createUser, login } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
